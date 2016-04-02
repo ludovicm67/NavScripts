dec84952d1a401b77c92aea82adba1df81789c39
Ludovic Muller <ludovicm67@users.noreply.github.com>
2016-04-02 13:43:53 +0200

    Update AllowAdBlockerOnLeFigaro.user.js

--- AllowAdBlockerOnLeFigaro.user.js
+++ AllowAdBlockerOnLeFigaro.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AllowAdBlockerOnLeFigaro
 // @namespace    @ludovicm67
-// @version      1.0.4
+// @version      1.0.5
 // @description  Let's allow AdBlocker on LeFigaro again !
 // @author       @ludovicm67
 // @include      *.lefigaro.fr/**
@@ -23,7 +23,7 @@ for(var i=0; i<blurredText.length; i++) {
 }
 
 // We remove all the zone for ads
-var adsZone = document.querySelectorAll('.fig-adgps, .pave-bg, .pub-carrousel-mini, .pub-zoom-react, .fig-gps-placeholder, .pub-zoom, .s24-shopping, .s24-art-pub-top, .s24-pub');
+var adsZone = document.querySelectorAll('.fig-adgps, .pave-bg, .pub-carrousel-mini, .pub-zoom-react, .fig-gps-placeholder, .pub-zoom, .s24-shopping, .s24-art-pub-top, .s24-pub, .fig-promo, .mad__shoppingbox, .mad__market__droit.mad__market__droit__article, .pane-bloc-ad, .pub-icone');
 for(var i=0; i<adsZone.length; i++) {
     adsZone[i].style.display = 'none';
 }
