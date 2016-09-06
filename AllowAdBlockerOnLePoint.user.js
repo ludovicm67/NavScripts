095efd7f5dbe0ca8cd223728a80ceafb3e424de1
Ludovic Muller <ludovicm67@users.noreply.github.com>
2016-09-06 10:51:14 +0200

    Update AllowAdBlockerOnLePoint.user.js

--- AllowAdBlockerOnLePoint.user.js
+++ AllowAdBlockerOnLePoint.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AllowAdBlockerOnLePoint
 // @namespace    @ludovicm67
-// @version      1.0.1
+// @version      1.0.2
 // @description  Let's allow AdBlocker on LePoint !
 // @author       @ludovicm67
 // @include      *.lepoint.fr/**
@@ -13,8 +13,10 @@
     'use strict';
 
     // We remove warnings about the fact that we are using an AdBlocker
-    var noAdsZone = document.querySelector('a[href*="/html/infos"]').parentNode;
-    noAdsZone.parentNode.removeChild(noAdsZone);
+    if(document.querySelector('a[href*="/html/infos"]')) {
+        var noAdsZone = document.querySelector('a[href*="/html/infos"]').parentNode;
+        noAdsZone.parentNode.removeChild(noAdsZone);
+    }
     
     // We remove most of zones for ads, ...
     var adsZone = document.querySelectorAll('.footer-market, .button-type, .bg-yellow');
