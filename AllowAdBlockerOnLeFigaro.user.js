5a0cf60b9b71f48a6e5c44e8878ce12d973c6dbd
Ludovic Muller <ludovicm67@users.noreply.github.com>
2016-09-10 10:41:30 +0200

    Update AllowAdBlockerOnLeFigaro.user.js

--- AllowAdBlockerOnLeFigaro.user.js
+++ AllowAdBlockerOnLeFigaro.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AllowAdBlockerOnLeFigaro
 // @namespace    @ludovicm67
-// @version      1.0.8
+// @version      1.0.9
 // @description  Let's allow AdBlocker on LeFigaro again !
 // @author       @ludovicm67
 // @include      *.lefigaro.fr/**
@@ -12,7 +12,7 @@
 // We remove warnings about the fact that we are using an AdBlocker
 var warningMessages = document.querySelectorAll('a[href*="comment-desactiver-facilement-votre-bloqueur-de-publicites-sur-le-figarofr.php"]');
 for(var i=0; i<warningMessages.length; i++) {
-    warningMessages[i].style.display = 'none';
+    warningMessages[i].parentNode.removeChild(warningMessages[i]);
 }
 
 // We make the articles readable again (and without disabling the AdBlocker)
@@ -23,9 +23,9 @@ for(var i=0; i<blurredText.length; i++) {
 }
 
 // We remove most of zones for ads
-var adsZone = document.querySelectorAll('.fig-col--pub, .fig-premium-abo, .fig-adgps, .pave-bg, .pub-carrousel-mini, .pub-zoom-react, .fig-gps-placeholder, .pub-zoom, .s24-shopping, .s24-art-pub-top, .s24-pub, .fig-promo, .mad__shoppingbox, .mad__market__droit.mad__market__droit__article, .pane-bloc-ad, .pub-icone, .fig-ad, .OUTBRAIN, .tvm-ad');
+var adsZone = document.querySelectorAll('.fig-art-col-right, .fig-col--pub, .fig-premium-abo, .fig-adgps, .pave-bg, .pub-carrousel-mini, .pub-zoom-react, .fig-gps-placeholder, .pub-zoom, .s24-shopping, .s24-art-pub-top, .s24-pub, .fig-promo, .mad__shoppingbox, .mad__market__droit.mad__market__droit__article, .pane-bloc-ad, .pub-icone, .fig-ad, .OUTBRAIN, .tvm-ad');
 for(var i=0; i<adsZone.length; i++) {
-    adsZone[i].style.display = 'none';
+    adsZone[i].parentNode.removeChild(adsZone[i]);
 }
 
 // We adjusts some elements width
